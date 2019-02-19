3b7eab4a61fdf5589d3f0db438061eaaa76b0f9f
mikkoziel <mkoziel045@gmail.com>
2019-02-19 01:38:33 +0100

    Temporary fix for testing purposes

--- app/src/main/java/interactor/QuestionInteractor.java
+++ app/src/main/java/interactor/QuestionInteractor.java
@@ -40,8 +40,8 @@ public class QuestionInteractor {
     }
 
     private ArrayList<Integer> selectQuestionID(Game game, Profile profile) throws SQLException {
-        String query = "select questionID from Question where gameID= " + game.getID() +
-                " and questionID not in (select questionID from Log where profilID = " + profile.getID() + ")";
+        String query = "select questionID from Question where gameID= " + game.getID() ;
+//                + " and questionID not in (select questionID from Log where profilID = " + profile.getID() + ")";
 
         ResultSet res = _dbConnector.runQuery(query);
         ArrayList<Integer> ids = new ArrayList<>();
